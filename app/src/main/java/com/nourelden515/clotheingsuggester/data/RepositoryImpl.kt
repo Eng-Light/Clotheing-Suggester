d3c2eb433a9aa30caf55276308b49c8a7fcd807a
Nour Elden Alaa <nourelden515@gmail.com>
2023-04-25 22:36:13 +0200

    enhance code

--- app/src/main/java/com/nourelden515/clotheingsuggester/data/RepositoryImpl.kt
+++ app/src/main/java/com/nourelden515/clotheingsuggester/data/RepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.nourelden515.clotheingsuggester.data
 
 import com.nourelden515.clotheingsuggester.data.source.RemoteDataSource
 import com.nourelden515.clotheingsuggester.utils.shared.SharedPreferencesInterface
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import okhttp3.Response
 
